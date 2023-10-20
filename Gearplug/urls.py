b119ce2dee2bf487dc7651c6df5c98b3e9a02a38
nervencid <nervencid@gmail.com>
2023-10-19 22:09:46 -0500

    Agregada documentacion swagger

--- Gearplug/urls.py
+++ Gearplug/urls.py
@@ -16,10 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from rest_framework.authtoken import views
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+#Configuracion para swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Popketasks API",
+      default_version='v1',
+      description="Esta aplicacion permite crear y administrar pokemones",
+      terms_of_service="",
+      contact=openapi.Contact(email="nervencid@gmail.com"),
+      license=openapi.License(name="GPL License"),
+   ),
+   public=True,
+   #permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Poketasks.urls')),
-    #path('api-token-auth/', views.obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
